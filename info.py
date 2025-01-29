78448d2547b1b2902a41c03948da163a99bcf7b7
sandy2k25 <sandy_2k25@proton.me>
2025-01-29 17:52:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,10 +27,10 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002417153344'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5287262394 6799568711').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sandy2k25:sandy_2k25proton.me@cluster0.vrwqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "sandy2k25")
 
 # Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', True)) # Set True Or False
+SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
